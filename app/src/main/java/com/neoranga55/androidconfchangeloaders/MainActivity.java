de0dbeb69766b13ec0c3a10c216d30b925877315
Sebas LG <neoranga55@gmail.com>
2016-04-03 23:44:03 +0200

    Add ButterKnife

--- app/src/main/java/com/neoranga55/androidconfchangeloaders/MainActivity.java
+++ app/src/main/java/com/neoranga55/androidconfchangeloaders/MainActivity.java
@@ -2,12 +2,43 @@ package com.neoranga55.androidconfchangeloaders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements DemoContract.ViewActions {
+
+    @Bind(R.id.tv_content) TextView mContentText;
+    @Bind(R.id.tv_loading) TextView mLoadingText;
+    @Bind(R.id.tv_request_cancelled) TextView mRequestCancelledText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void showLoading() {
+        mContentText.setVisibility(View.GONE);
+        mRequestCancelledText.setVisibility(View.GONE);
+        mLoadingText.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void showContentLoaded() {
+        mLoadingText.setVisibility(View.GONE);
+        mRequestCancelledText.setVisibility(View.GONE);
+        mContentText.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void showCancelledRequest() {
+        mContentText.setVisibility(View.GONE);
+        mLoadingText.setVisibility(View.GONE);
+        mRequestCancelledText.setVisibility(View.VISIBLE);
     }
 }
