196addf4cd884d140c051c73bc0d861324d741ad
Sebas LG <neoranga55@gmail.com>
2016-04-04 00:35:48 +0200

    Connect Activity(View) with Loader and Presenter

--- app/src/main/java/com/neoranga55/androidconfchangeloaders/MainActivity.java
+++ app/src/main/java/com/neoranga55/androidconfchangeloaders/MainActivity.java
@@ -1,26 +1,58 @@
 package com.neoranga55.androidconfchangeloaders;
 
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.neoranga55.androidconfchangeloaders.presenters.DemoContract;
+import com.neoranga55.androidconfchangeloaders.presenters.DemoPresenter;
+import com.neoranga55.androidconfchangeloaders.presenters.DemoPresenterFactory;
+import com.neoranga55.androidconfchangeloaders.presenters.Presenter;
+import com.neoranga55.androidconfchangeloaders.presenters.PresenterLoader;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements DemoContract.ViewActions {
 
+    private static final String TAG = "MainActivity";
+    private static final int LOADER_ID = 101;
     @Bind(R.id.tv_content) TextView mContentText;
     @Bind(R.id.tv_loading) TextView mLoadingText;
     @Bind(R.id.tv_request_cancelled) TextView mRequestCancelledText;
+    private DemoContract.UserActions<DemoContract.ViewActions> mUserActions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        // LoaderCallbacks as an object, so no hint regarding Loader will be leak to the subclasses.
+        getSupportLoaderManager().initLoader(LOADER_ID, null, new LoaderManager.LoaderCallbacks<DemoPresenter>() {
+            @Override
+            public final Loader<DemoPresenter> onCreateLoader(int id, Bundle args) {
+                Log.i(TAG, "onCreateLoader");
+                return new PresenterLoader<>(MainActivity.this, new DemoPresenterFactory(), TAG);
+            }
+
+            @Override
+            public final void onLoadFinished(Loader<DemoPresenter> loader, DemoPresenter presenter) {
+                Log.i(TAG, "onLoadFinished");
+                MainActivity.this.mUserActions = presenter;
+//                onPresenterPrepared(presenter);
+            }
+
+            @Override
+            public final void onLoaderReset(Loader<DemoPresenter> loader) {
+                Log.i(TAG, "onLoaderReset");
+                MainActivity.this.mUserActions = null;
+//                onPresenterDestroyed();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/neoranga55/androidconfchangeloaders/presenters/DemoPresenterFactory.java
+++ app/src/main/java/com/neoranga55/androidconfchangeloaders/presenters/DemoPresenterFactory.java
@@ -3,7 +3,7 @@ package com.neoranga55.androidconfchangeloaders.presenters;
 /**
  * Created by neoranga on 04/04/2016.
  */
-public class DemoPresenterFactory implements PresenterFactory<DemoPresenter>{
+public class DemoPresenterFactory implements PresenterFactory<DemoPresenter> {
 
     @Override
     public DemoPresenter create() {
