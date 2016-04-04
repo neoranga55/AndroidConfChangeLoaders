2cd6f1035a32a07472f46c20614fea65ae3a387d
Sebas LG <neoranga55@gmail.com>
2016-04-05 00:15:43 +0200

    Save and restore state of view on attach to presenter

--- app/src/main/java/com/neoranga55/androidconfchangeloaders/presenters/DemoPresenter.java
+++ app/src/main/java/com/neoranga55/androidconfchangeloaders/presenters/DemoPresenter.java
@@ -1,7 +1,5 @@
 package com.neoranga55.androidconfchangeloaders.presenters;
 
-import android.os.AsyncTask;
-
 /**
  * Created by neoranga on 28/03/2016.
  */
@@ -9,10 +7,22 @@ public class DemoPresenter implements DemoContract.UserActions<DemoContract.View
 
     private DemoContract.ViewActions mViewActions;
     private Thread mSlowTask;
+    private boolean isLoading;
+    private boolean isContentLoaded;
+    private boolean isLoadCancelled;
 
     @Override
     public void onViewAttached(DemoContract.ViewActions viewActions) {
         mViewActions = viewActions;
+        if (isLoading) {
+            mViewActions.showLoading();
+        }
+        if (isContentLoaded) {
+            mViewActions.showContentLoaded();
+        }
+        if (isLoadCancelled) {
+            mViewActions.showCancelledRequest();
+        }
     }
 
     @Override
@@ -27,13 +37,19 @@ public class DemoPresenter implements DemoContract.UserActions<DemoContract.View
 
     @Override
     public void loadButtonPressed() {
+        isLoading = true;
+        isContentLoaded = false;
+        isLoadCancelled = false;
         mViewActions.showLoading();
         mSlowTask = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     Thread.sleep(30000);
+                    isLoading = false;
+                    isContentLoaded = true;
                     mViewActions.showContentLoaded();
+                    mSlowTask = null;
                 } catch (InterruptedException ignored) {
                 }
             }
@@ -43,9 +59,13 @@ public class DemoPresenter implements DemoContract.UserActions<DemoContract.View
 
     @Override
     public void cancelButtonPressed() {
+        isLoading = false;
         if (mSlowTask != null) {
+            isLoadCancelled = true;
+            isContentLoaded = false;
             mSlowTask.interrupt();
+            mSlowTask = null;
+            mViewActions.showCancelledRequest();
         }
-        mViewActions.showCancelledRequest();
     }
 }
